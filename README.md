6b584e0de86539a00ccebb39c080d97502efbdc0
goutarouh <gouta.gorogoro@gmail.com>
2023-05-22 23:10:37 +0900

    host二つとrouterの接続確認

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ https://twitter.com/kametan0730
 
 本記事の著者
 https://twitter.com/ken5owata
+
+- メモ
+  https://zenn.dev/gottie/scraps/ff460572e29319
---
+++ env/section1_host1_router1_host2.sh
@@ -0,0 +1,31 @@
+sudo ip netns add router1
+sudo ip netns add host1
+sudo ip netns add host2
+
+sudo ip link add host1-router1 type veth peer name router1-host1
+sudo ip link add host2-router1 type veth peer name router1-host2
+
+sudo ip link set host1-router1 netns host1
+sudo ip link set router1-host1 netns router1
+sudo ip link set host2-router1 netns host2
+sudo ip link set router1-host2 netns router1
+
+sudo ip netns exec host1 ip link set host1-router1 up
+sudo ip netns exec router1 ip link set router1-host1 up
+sudo ip netns exec host2 ip link set host2-router1 up
+sudo ip netns exec router1 ip link set router1-host2 up
+
+sudo ip netns exec host1 ip addr add 10.0.1.2/24 dev host1-router1
+sudo ip netns exec router1 ip addr add 10.0.1.254/24 dev router1-host1
+sudo ip netns exec host2 ip addr add 192.0.1.2/24 dev host2-router1
+sudo ip netns exec router1 ip addr add 192.0.1.254/24 dev router1-host2
+
+sudo ip netns exec host1 ip route add 192.0.1.2 via 10.0.1.254
+sudo ip netns exec host2 ip route add 10.0.1.2 via 192.0.1.254
+
+sudo ip netns exec router1 sysctl net.ipv4.ip_forward=1
+
+# お掃除
+# sudo ip netns exec host1 ip link del host1-router1;sudo ip netns exec host2 ip link del host2-router1;sudo ip netns del host1;sudo ip netns del host2;sudo ip netns del router1
+
+
